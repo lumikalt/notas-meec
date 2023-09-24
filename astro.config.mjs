a5fcb7203bcf2b7c15bb3f7f230be4ba0fb2faf9
Lumi Kalt <lumi.mia.kalt@gmail.com>
2023-09-24 13:44:29 +0100

    Remove unnecessary config

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,16 +10,6 @@ export default defineConfig({
     site: "https://lumikalt.vercel.app",
     sitemap: true,
     integrations: [mdx(), sitemap()],
-    mdx: {
-        remarkPlugins: [remarkMath, remarkToc],
-        rehypePlugins: [[
-            rehypeMahJax,
-            {
-                loader: { load: ["[tex]/physics", "[tex]/ams"] },
-                tex: { packages: { "[+]": ["physics", "ams"] } }
-            }
-        ]]
-    },
     markdown: {
         remarkPlugins: [remarkMath, remarkToc],
         rehypePlugins: [[
